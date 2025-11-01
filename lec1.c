607e6be269781e235853768d2caf7c42405f268c
Kavitakushwaha004 <sakshikushwaha8823@gmail.comgit config --listgit config --global user.email sakshikushwaha8823@gmail.comgit config --list git config --global user.email sakshikushwaha8823@gmail.comgit config --list  git config --global user.email sakshikushwaha8823@gmail.comgit config --global user.email sakshikushwaha8823@gmail.com>
2025-11-01 11:56:46 -0700

    added lec1.c file

---
+++ lec1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int age=20;
+  float pi=3.14;
+  char grade='A';
+
+  printf("Hello Students\n");
+  printf("Age=%d\n",age);
+printf("pi=%f\n",pi);
+printf("grade=%c",grade);
+
+return 0;
+}
